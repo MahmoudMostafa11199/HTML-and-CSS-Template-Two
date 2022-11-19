a12b65f8e201fc93231514d7232d3c95f9fa7874
Mahmoud <m_devolepment@hotmail.com>
2022-11-19 02:49:49 +0200

    added javascript

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("Hello world!");
 ///////////////////////////////////////////////////////////
 // Current Year
 
@@ -6,8 +5,42 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+
+
 ///////////////////////////////////////////////////////////
-const submit = document.querySelector(".submit");
-submit.addEventListener("click", function (e) {
-  e.preventDefault();
-});
+// Mobile Nav
+
+const btnNav=document.querySelector(".btn-mobile-nav")
+const headerEl=document.querySelector(".header")
+
+btnNav.addEventListener("click",function(){
+	headerEl.classList.toggle("nav-open")
+})
+
+///////////////////////////////////////////////////////////
+// Sticky Nav
+
+const landingEl =document.querySelector(".landing");
+
+const obs = new IntersectionObserver(
+	function (entries) {
+		const ent = entries[0];
+		console.log(ent);
+
+		if (!ent.isIntersecting) {
+			document.body.classList.add("sticky")
+			// landingEl.classList.add("sticky")
+		}
+
+		if (ent.isIntersecting) {
+			document.body.classList.remove("sticky")
+			// landingEl.classList.remove("sticky")
+		}
+	},
+	{
+		root: null,
+		threshold : 0,
+		rootMargin: "-80px",
+	})
+
+obs.observe(landingEl);
