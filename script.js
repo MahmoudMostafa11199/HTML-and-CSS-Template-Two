983e25d09d5ae7366dc81875f0754af5431b088d
Mahmoud <m_devolepment@hotmail.com>
2022-11-19 03:16:12 +0200

    update

--- script.js
+++ script.js
@@ -44,3 +44,33 @@ const obs = new IntersectionObserver(
 	})
 
 obs.observe(landingEl);
+
+///////////////////////////////////////////////////////////
+// Smoth Scrolling
+
+allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function(link) {
+	link.addEventListener("click", function(e) {
+		e.preventDefault();
+
+		const href = link.getAttribute("href");
+
+		// Scroll back to top
+		if (href === "#")
+      		window.scrollTo({
+        	top: 0,
+        	behavior: "smooth",
+		    });
+
+	    // Scroll to other links
+	    if (href !== "#" && href.startsWith("#")) {
+	      const sectionEl = document.querySelector(href);
+	      sectionEl.scrollIntoView({ behavior: "smooth" });
+	    }
+
+	    // Close mobile naviagtion
+	    if (link.classList.contains("nav-link"))
+	      headerEl.classList.toggle("nav-open");
+	})
+})
